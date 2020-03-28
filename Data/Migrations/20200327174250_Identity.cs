be24fe4310d75e41c37422adc939cf7389386392
SashoTatarski <sasho56@hotmail.com>
2020-03-28 12:58:31 +0200

    Identity added

---
+++ Data/Migrations/20200327174250_Identity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DutchTreat.Migrations
+{
+    public partial class Identity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2020, 3, 27, 17, 42, 50, 399, DateTimeKind.Utc).AddTicks(6293));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2020, 3, 26, 7, 46, 31, 222, DateTimeKind.Utc).AddTicks(6530));
+        }
+    }
+}
